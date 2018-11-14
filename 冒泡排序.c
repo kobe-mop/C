6d563759d2ce6a228f616ff702219132686a8246
The X-factor <kobe-mop@qq.com>
2018-11-14 10:49:33 +0800

    Update 冒泡排序.c

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -3,7 +3,7 @@
 void bubble(int[] arr, int length){
 	for(int i =0; i<length-1; i++)
 	{	
-		bool exchange = false;
+	        bool exchange = false;
 		for(int j = 0; j<length-i-1; j++)
 		{
 			if(arr[j]>arr[j+1])
@@ -12,12 +12,6 @@ void bubble(int[] arr, int length){
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
 				exchange = true;
-				
-				/*
-				a[j]=a[j]+a[j+1];
-				a[j+1]=a[j]-a[j+1];
-				a[j]=a[j]-a[j+1]				
-				*/
 			}
 		}
 		if(!exchange)

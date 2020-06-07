8f4ff046f48b02d10c1cfc089c20fdebc3e401c5
The X-factor <kobe-mop@qq.com>
2020-06-07 15:53:31 +0800

    Update 链表的逆序_合并.cc

--- 链表的逆序_合并.cc
+++ 链表的逆序_合并.cc
@@ -41,109 +41,55 @@ class Solution{
 
 
 
-//翻转数组
-#include <iostream>
-using namespace std; 
-void reversal(int arr[], int n)
-{
-    int temp;
- 
-    for(int i=0; i<n/2; i++)
-    {
-        temp = arr[i];
-        arr[i] = arr[n-1-i];
-        arr[n-1-i] = temp;
-    }
-}
-void list(int arr[], int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        cout<<arr[i]<<"  ";
-    }
-    cout<<endl;
-}
-int main() 
-{
-    int arr[] = {78, 86, 90, 83, 74, 68, 74, 80, 69, 79, 89, 93};
-    int n = 12;
-     
-    cout<<"翻转前："<<endl;
-    list(arr, n);
- 
-    reversal(arr, n);
- 
-    cout<<"翻转后："<<endl;
-    list(arr, n);
- 
-    return 0;
-}
 
 
 
 //有序链表合并
-void* mergeList(Node *head1, Node *head2){
-	if(head1 == NULL)
-		return head2;
-	if(head2 == NULL)
-		return head1;
-	
-	Node *head = NULL;
-	Node *p1 = NULL;
-	Node *p2 = NULL;
-	
-	if(head1->data <= head2->data){
-		head = head1;
-		p1 = head1->next;
-		p2= head2;
-	}
-	else{
-		head = head2;
-		p2 = head2->next;
-		p1 = head1;
-	}
-	
-	Node *pcurrent = head;
-	while(p1 !=NULL && p2 != NULL)
-	{
-		if(p1->data <= p2->data){
-			pcurrent->next = p1;
-			pcurrent = p1;
-			p1= p1->next;
-			}
-		else{
-			pcurrent->next = p2;
-			pcurrent = p2;
-			p2= p2->next;
-		}
-	}
-	
-	if(p1 == NULL)
-		pcurrent->next = p2;
-	
-	if(p2 == NULL)
-		pcurrent->next = p1;
-	
-	return head;	
-}
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(NULL) {}
+      ListNode(int x) : val(x), next(NULL) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
 
-//有序链表递归方法合并
-void* mergeListRecursive(Node *head1, Node *head2){
-	if(head1 == NULL)
-		return head2;
-	if(head2 == NULL)
-		return head1;
-	
-	Node *head = NULL;
-	
-	if(head1->data <= head2->data){
-		head = head1;
-		head->next = mergeListRecursive(head1->next,head2);
-	}
-	else{
-		head = head2;
-		head->next = mergeListRecursive(head1,head2->next);
-	}
-	
-	return head;	
-}
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(-1);
+        ListNode *pre = head;
+
+        while(l1 != NULL && l2 != NULL){
+            if(l1->val<l2->val){
+                pre->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                pre->next = l2;
+                l2 = l2->next;
+            }
+            pre = pre->next;
+        }
+
+        pre->next = l1 == NULL ? l2 : l1;
+        return head->next;
+
+        //递归方法
+        // if(l1 == NULL){
+        //     return l2;
+        // }
+        // else if (l2 == NULL)
+        // {
+        //     return l1;
+        // }
+        // else if(l1->val<l2->val){
+        //     l1->next = mergeTwoLists(l1->next, l2);
+        //     return l1;
+        // }
+        // else {
+        //     l2->next = mergeTwoLists(l1, l2->next);
+        //     return l2;
+        // }
+    }
+};
